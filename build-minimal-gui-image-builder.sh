57a5042109bc268fca696c21043e3335bb0e7591
Lunar <lunarlifeburst@gmail.com>
2026-04-22 01:53:49 +1000

    remove unneeded script

--- build-minimal-gui-image-builder.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-source "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)/scripts/common.sh"
-
-require_secrets
-cd_repo_root
-
-exec nix build -v "path:.#nixosConfigurations.myhost-minimal.config.system.build.diskoImagesScript"
